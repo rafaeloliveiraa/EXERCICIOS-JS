8357bc8f2fb5db6a0b2739c6ac9e310b2657360a
Rafael de Oliveira <rafaeldeoliveira523@gmail.com>
2024-02-01 21:46:28 -0300

    Commit

---
+++ controle/while.js
@@ -0,0 +1,17 @@
+// Apropriada para uma quantidade indeterminada de repetições.
+
+// Enquanto a expressão for verdadeira faça, sai do laço quando der false.
+
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a próxima!')
